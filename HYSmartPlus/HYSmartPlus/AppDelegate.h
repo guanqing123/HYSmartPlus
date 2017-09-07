d4102c9f8f73e39a5b649d94611e0b1c821cb769
guanqing123 <information@wmzdeMacBook-Pro.local>
2017-09-07 22:48:29 +0800

    初始化项目

---
+++ HYSmartPlus/HYSmartPlus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYSmartPlus
+//
+//  Created by information on 2017/9/7.
+//  Copyright © 2017年 hongyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
