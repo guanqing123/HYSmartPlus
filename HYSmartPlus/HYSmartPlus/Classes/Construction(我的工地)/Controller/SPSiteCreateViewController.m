d2853a75752237a33eba1cb166ff84e945ffc7f0
guanqing123 <guanqinghz@163.com>
2018-05-02 23:50:50 +0800

    完善我的工地

--- HYSmartPlus/HYSmartPlus/Classes/Construction(我的工地)/Controller/SPSiteCreateViewController.m
+++ HYSmartPlus/HYSmartPlus/Classes/Construction(我的工地)/Controller/SPSiteCreateViewController.m
@@ -8,7 +8,22 @@
 
 #import "SPSiteCreateViewController.h"
 
-@interface SPSiteCreateViewController ()
+#import "UIView+Layout.h"
+#import "TZTestCell.h"
+
+#import "LxGridViewFlowLayout.h"
+
+@interface SPSiteCreateViewController ()<UICollectionViewDataSource,UICollectionViewDelegate> {
+    NSMutableArray *_selectedPhotos;
+    NSMutableArray *_selectedAssets;
+    
+    CGFloat _itemWH;
+    CGFloat _margin;
+}
+@property (nonatomic, strong)  UICollectionView *collectionView;
+@property (nonatomic, strong)  LxGridViewFlowLayout *layout;
+
+@property (nonatomic, assign) BOOL allowPickingGif;
 
 @end
 
@@ -17,21 +32,69 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    // 1.导航栏等初始化
+    [self setupNavBar];
+    // 2.CollectionView
+    [self setupCollectionView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    
+    _margin = 4;
+    _itemWH = (self.view.tz_width - 2 * _margin - 4) / 3 - _margin;
+    _layout.itemSize = CGSizeMake(_itemWH, _itemWH);
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - setupNavBar
+- (void)setupNavBar {
+    self.title = @"工地信息";
+    
+    _selectedPhotos = [NSMutableArray array];
+    _selectedAssets = [NSMutableArray array];
+    
+    _allowPickingGif = YES;
 }
-*/
+
+#pragma mark - collectionView
+- (void)setupCollectionView {
+    // 如不需要长按排序效果，将LxGridViewFlowLayout类改成UICollectionViewFlowLayout即可
+    _layout = [[LxGridViewFlowLayout alloc] init];
+    _collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:_layout];
+    _collectionView.alwaysBounceVertical = YES;
+    _collectionView.backgroundColor = RGB(244, 244, 244);
+    _collectionView.contentInset = UIEdgeInsetsMake(4, 4, 4, 4);
+    _collectionView.dataSource = self;
+    _collectionView.delegate = self;
+    _collectionView.keyboardDismissMode = UIScrollViewKeyboardDismissModeOnDrag;
+    [self.view addSubview:_collectionView];
+    [_collectionView registerClass:[TZTestCell class] forCellWithReuseIdentifier:@"TZTestCell"];
+}
+
+#pragma mark - UICollectionViewDataSource
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return _selectedPhotos.count + 1;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    TZTestCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"TZTestCell" forIndexPath:indexPath];
+    cell.videoImageView.hidden = YES;
+    if (indexPath.row == _selectedPhotos.count) {
+        cell.imageView.image = [UIImage imageNamed:@"AlbumAddBtn.png"];
+        cell.deleteBtn.hidden = YES;
+        cell.gifLable.hidden = YES;
+    } else {
+        cell.imageView.image = _selectedPhotos[indexPath.row];
+        cell.asset = _selectedAssets[indexPath.row];
+        cell.deleteBtn.hidden = NO;
+    }
+    if (!self.allowPickingGif) {
+        cell.gifLable.hidden = YES;
+    }
+    cell.deleteBtn.tag = indexPath.row;
+    [cell.deleteBtn addTarget:self action:@selector(deleteBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+    return cell;
+}
 
 @end
